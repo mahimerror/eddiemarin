d5d442c216fdc518c7513dfa901d28c87e138264
mahimerror <mahimislam46@gmail.com>
2025-04-06 17:59:49 +0600

    fix shadcn accordion

--- src/components/ui/accordion.jsx
+++ src/components/ui/accordion.jsx
@@ -8,7 +8,14 @@ import { FaMinus, FaPlus } from "react-icons/fa";
 const Accordion = AccordionPrimitive.Root;
 
 const AccordionItem = React.forwardRef(({ className, ...props }, ref) => (
-  <AccordionPrimitive.Item ref={ref} className={cn("", className)} {...props} />
+  <AccordionPrimitive.Item
+    ref={ref}
+    className={cn(
+      "p-6 [&[data-state=open]]:shadow-[0px_8px_24px_0px_rgba(0,66,153,0.08)] rounded-xl",
+      className
+    )}
+    {...props}
+  />
 ));
 AccordionItem.displayName = "AccordionItem";
 
@@ -18,13 +25,13 @@ const AccordionTrigger = React.forwardRef(
       <AccordionPrimitive.Trigger
         ref={ref}
         className={cn(
-          "flex flex-1 items-center justify-between py-4 font-medium transition-all hover:underline group [&[data-state=open]]:border",
+          "flex flex-1 items-center justify-between transition-all group text-[#043E5C] font-semibold text-2xl leading-[150%]",
           className
         )}
         {...props}
       >
         {children}
-        <div className="relative size-9 rounded-full shrink-0 border border-[#0B0B0B] text-[#0B0B0B] group-data-[state=open]:text-white bg-transparent group-data-[state=open]:bg-primary group-data-[state=open]:border-primary transition-colors duration-300">
+        <div className="relative size-9 rounded-full shrink-0 border-2 border-primary text-primary group-data-[state=open]:text-white bg-transparent group-data-[state=open]:bg-primary group-data-[state=open]:border-primary transition-colors duration-300">
           <FaPlus className="absolute size-[18px] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 transition-transform duration-300 group-data-[state=closed]:opacity-100 group-data-[state=closed]:rotate-0 group-data-[state=open]:opacity-0 group-data-[state=open]:rotate-180   " />
           <FaMinus className="absolute size-[18px] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 transition-transform duration-300 group-data-[state=closed]:opacity-0 group-data-[state=closed]:-rotate-180 group-data-[state=open]:opacity-100 group-data-[state=open]:rotate-0" />
         </div>
@@ -38,10 +45,10 @@ const AccordionContent = React.forwardRef(
   ({ className, children, ...props }, ref) => (
     <AccordionPrimitive.Content
       ref={ref}
-      className="overflow-hidden text-sm transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down"
+      className="overflow-hidden text-lg font-medium transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down leading-[150%] text-[#6A7283]"
       {...props}
     >
-      <div className={cn("pb-4 pt-0", className)}>{children}</div>
+      <div className={cn("pt-4", className)}>{children}</div>
     </AccordionPrimitive.Content>
   )
 );
--- src/pages/public/Faq.jsx
+++ src/pages/public/Faq.jsx
@@ -14,21 +14,30 @@ const Faq = () => {
         Frequently Asked Questions
       </SectionTitle>
       <Container>
-        <Accordion type="single" collapsible>
+        <Accordion type="single" defaultValue="item-1" collapsible>
           <AccordionItem value="item-1">
-            <AccordionTrigger>Is it accessible?</AccordionTrigger>
+            <AccordionTrigger>
+              1. What does an Earnest Money Deposit (EMD) mean to ‘Terms For
+              Sale’?
+            </AccordionTrigger>
             <AccordionContent>
-              Yes. It adheres to the WAI-ARIA design pattern.
+              A: The Earnest Money Deposit is a deposit made by the buyer after
+              accepting the terms of the assignment. This is the only way to
+              secure their interest in the property once they have signed the
+              binding contract.
             </AccordionContent>
           </AccordionItem>
           <AccordionItem value="item-2">
-            <AccordionTrigger>Is it accessible?</AccordionTrigger>
+            <AccordionTrigger>
+              2. Why is the EMD non-refundable when working with ‘Terms For
+              sale’?{" "}
+            </AccordionTrigger>
             <AccordionContent>
               Yes. It adheres to the WAI-ARIA design pattern.
             </AccordionContent>
           </AccordionItem>
           <AccordionItem value="item-3">
-            <AccordionTrigger>Is it accessible?</AccordionTrigger>
+            <AccordionTrigger>3. Why is it important to send the EMD promptly? </AccordionTrigger>
             <AccordionContent>
               Yes. It adheres to the WAI-ARIA design pattern.
             </AccordionContent>
